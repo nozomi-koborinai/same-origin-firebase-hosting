afb5de91d9d27184f821eddcb7dd875dcaed41b9
nozomi-koborinai <kobofender@gmail.com>
2024-05-06 17:27:08 +0900

    chore: app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import { query } from './postgres';
 
 const app: Application = express();
 
@@ -9,6 +10,16 @@ app.get("/hello-world", async (req: Request, res: Response) => {
   return res.status(200).send({
     message: "Hello World!",
   });
+});
+
+app.get('/data', async (req: Request, res: Response) => {
+  try {
+    const result = await query('SELECT * FROM your_table_name');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
 });
 
 const port = process.env.PORT || 8000;
