38586a2df44f254e0cc986feabdf90064578fed9
nozomi-koborinai <kobofender@gmail.com>
2024-05-06 17:27:24 +0900

    feat: add postgres.ts

---
+++ backend/src/postgres.ts
@@ -0,0 +1,25 @@
+// postgres.ts
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT || "5432"),
+});
+
+export async function query(text: string, params?: any[]) {
+  const start = Date.now();
+  const res = await pool.query(text, params);
+  const duration = Date.now() - start;
+  console.log('executed query', { text, duration, rows: res.rowCount });
+  return res;
+}
+
+export async function shutdownDb() {
+  await pool.end();
+}
